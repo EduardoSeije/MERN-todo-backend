3beeca549bdfa3c04409b2b72c2ce56a0c5d4a6a
Eduardo Seije <eduardoseije@gmail.com>
2021-11-03 17:20:25 -0300

    Criação do model com validação dos dados feita pelo mongoos.Schema

---
+++ model/TodoModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: 'String',
+    required: [true, 'Por favor, insira um título' ],
+  },
+  description: {
+    type: 'String',
+    required: [true, 'Por favor, insira uma descrição']
+  }, 
+},
+  {
+    timestamps: true,
+  }
+);
+
+const Todo = mongoose.model('TodoModel', todoSchema);
+
+module.exports = { TodoModel }
