17376df7c9f7a8420dbfb28c0f2a21d6c4aa0113
Eduardo Seije <eduardoseije@gmail.com>
2021-11-03 16:29:59 -0300

    Criando a conexção com express

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log('hi')
+const express = require('express');
+
+const app = express();
+
+//Sever
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Ouvindo a porta ${PORT}`));
